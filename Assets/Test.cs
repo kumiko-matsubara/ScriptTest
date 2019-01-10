61fd908d295cde229efd3c54f68bf944dc8451eb
kumiko1203 <qoo@mrsk.co.jp>
2019-01-10 10:45:36 +0900

    よろしくお願いいたします。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,19 +6,12 @@ public class Test : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        //要素の個数が５の配列
-        int[] array = new int[5];
+        //配列の宣言と初期化をする
+        int[] array = { 30, 20, 50, 10, 80 };
 
 
-        //配列の各要素の値を代入する
-        array [0] = 30;
-        array [1] = 20;
-        array [2] = 50;
-        array [3] = 10;
-        array [4] = 80;
-
-        //配列の要素をすべて表示する
-        for (int i = 0; i < 5; i++) {
+        //配列の要素を逆順に表示
+        for (int i = array.Length - 1; i>=0; i--) { 
             Debug.Log (array[i]);
 
         }
