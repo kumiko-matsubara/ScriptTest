1ec339c0099a75d79fe97a676fb6b4014f044e76
kumiko1203 <qoo@mrsk.co.jp>
2019-01-11 10:53:34 +0900

    順番に表示した後に逆順を表示しました よろしくお願いします

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,9 +6,15 @@ public class Test : MonoBehaviour {
 
     // Use this for initialization
     void Start() {
-        //配列の宣言と初期化をする
-        int[] array = { 30, 20, 50, 10, 80 };
+        //要素数の個数が５の配列
+        int[] array = new int[5];
 
+        //配列の各要素に値を代入
+        array[0] = 30;
+        array[1] = 20;
+        array[2] = 50;
+        array[3] = 10;
+        array[4] = 80;
 
         //配列の要素を逆順に表示
         for (int i = array.Length - 1; i>=0; i--) { 
